e8f338e4bfdc2a5ffb8d2d3af961b8779675fcdf
iamlejon <niquelejon@gmail.com>
2018-09-08 01:33:25 -0500

    Added html and js page for lucky sevens project, created placeBetGreeting js function

---
+++ portfolio/luckySevens/scripts/luckySevens.js
@@ -0,0 +1,4 @@
+function placeBetGreeting() {
+    var playerBet = prompt("Welcome to Lucky Sevens \n + Go Ahead, Place Your Bet!");
+    document.getElementById("userName").innerText = userName;
+}
