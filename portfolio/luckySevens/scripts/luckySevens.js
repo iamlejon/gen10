4bd5baa7ee3fe32dac1062bd53f084cfc8c5a2c0
iamlejon <niquelejon@gmail.com>
2018-09-08 13:48:10 -0500

    Updated lucky Sevens js file placeBetGreeting and validation.  Added Roll Dice function

--- portfolio/luckySevens/scripts/luckySevens.js
+++ portfolio/luckySevens/scripts/luckySevens.js
@@ -1,4 +1,27 @@
+var playerBet;
 function placeBetGreeting() {
-    var playerBet = parseFloat(prompt("Welcome to Lucky Sevens \n Go Ahead, Place Your Bet!")).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');;
-    document.getElementById("startingBet").innerText = playerBet;
+    var playerBet = parseFloat(prompt("Enter your bet")).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+    if(playerBet <= 0 || playerBet=="" || isNaN(playerBet)){
+        alert("You must enter a numeric number greater than 0")
+        placeBetGreeting();
+        return false;
+    } else {
+        document.getElementById("startingBet").innerText = playerBet;
+        return false;
+    }
+    
 }
+
+var diceA = 0;
+var diceB = 0;
+
+
+function rollDice() {
+        diceA = Math.floor(Math.random() * 6) + 1;
+        diceB = Math.floor(Math.random() * 6) + 1;
+        document.getElementById("results").style.display = "block";
+        document.getElementById("resultsDisplay").innerText = (diceA+diceB);
+        return false;
+    }
+     
+  
