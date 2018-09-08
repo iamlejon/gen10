98ddf2f53187ff9316234773b0bc89ab157ee541
iamlejon <niquelejon@gmail.com>
2018-09-08 16:30:06 -0500

    Added a while loop to run while playerAmout is greater than 0.  Re-arranged code to put rollDice in while loop

--- portfolio/luckySevens/scripts/luckySevens.js
+++ portfolio/luckySevens/scripts/luckySevens.js
@@ -1,12 +1,14 @@
-var playerBet;
+var playerAmount;
+var startingBet;
 function placeBetGreeting() {
-    playerBet = parseFloat(prompt("Enter your bet")).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
-    if(playerBet <= 0 || playerBet=="" || isNaN(playerBet)){
+    startingBet = parseFloat(prompt("Enter your bet")).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+    playerAmount = parseFloat(startingBet);
+    if(playerAmount <= 0 || playerAmount=="" || isNaN(playerAmount)){
         alert("You must enter a numeric number greater than 0")
         placeBetGreeting();
         return false;
     } else {
-        document.getElementById("startingBet").innerText = playerBet;
+        document.getElementById("startingBet").innerText = startingBet;
         return false;
     }
     
@@ -14,14 +16,33 @@ function placeBetGreeting() {
 
 var diceA = 0;
 var diceB = 0;
+var sum;
+var totalRolls = 0;
+
 
 
 function rollDice() {
-        diceA = Math.floor(Math.random() * 6) + 1;
-        diceB = Math.floor(Math.random() * 6) + 1;
+        while(Number(playerAmount)>Number(0)){
+            diceA = Math.floor(Math.random() * 6) + 1;
+            diceB = Math.floor(Math.random() * 6) + 1;
+            sum = diceA+diceB;
+
+            if(sum==7) {
+                playerAmount = parseFloat(playerAmount) + parseFloat(4.00);
+            }
+            else {
+                playerAmount = parseFloat(playerAmount) - parseFloat(1.00);
+            }
+        
+            totalRolls++;
+        }
+       
+       
         document.getElementById("results").style.display = "block";
-        document.getElementById("startingBetDisplay").innerText = (playerBet);
-        document.getElementById("resultsDisplay").innerText = (diceA+diceB);
+        document.getElementById("current").innerText = (playerAmount);
+        document.getElementById("startingBetDisplay").innerText = (startingBet);
+        document.getElementById("rolls").innerText = totalRolls;
+        document.getElementById("resultsDisplay").innerText = (sum);
         return false;
     }
      
