4d5c560b475f24b799a02e878812872c73e264f8
iamlejon <niquelejon@gmail.com>
2018-09-08 20:09:24 -0500

    Re adjusted if else keyvalue pair statements to correct highest amount won and highest roll misunderstanding

--- portfolio/luckySevens/scripts/luckySevens.js
+++ portfolio/luckySevens/scripts/luckySevens.js
@@ -21,19 +21,22 @@ var totalRolls = 0;
 var winnings = 0;
 var pairs = {};
 
+function clearAll(){
+    totalRolls=0;
+    startingBet=0;
+    sum=0;
+    winnings=0;
+    pairs = {};
+    document.getElementById("results").style.display = "none";
+    document.getElementById("submitButton").innerText = "Play";
+    placeBetGreeting();
+    return false;
+}
 
 
 function rollDice() {
     if(playerAmount==0) {
-        totalRolls=0;
-        startingBet=0;
-        sum=0;
-        winnings=0;
-        pairs = {};
-        document.getElementById("results").style.display = "none";
-        document.getElementById("submitButton").innerText = "Play";
-        placeBetGreeting();
-        return false;
+        clearAll();
     }
         while(Number(playerAmount)>Number(0)){
             diceA = Math.floor(Math.random() * 6) + 1;
@@ -43,27 +46,32 @@ function rollDice() {
             if(sum==7) {
                 playerAmount = parseFloat(playerAmount) + parseFloat(4.00);
                 winnings += 4;
-                pairs[totalRolls] = winnings;
+                
             }
             else {
                 playerAmount = parseFloat(playerAmount) - parseFloat(1.00);
-                winnings=0;
+                winnings -= 1;
             }
         
             totalRolls++;
+            pairs[totalRolls] = winnings;
         }
+
+    
        
         let keys = Object.keys(pairs);
         keys.sort(function(a,b){return pairs[b]-pairs[a];});
         let rollCountAtHighestWon = keys[0];
         let highestWon = pairs[rollCountAtHighestWon];
+
+        var rollCountFinal = Object.is(rollCountAtHighestWon, undefined) ? 0 : rollCountAtHighestWon;
+        var highWonFinal = Object.is(highestWon, undefined) ? 0 : highestWon;
        
         document.getElementById("results").style.display = "block";
         document.getElementById("startingBetDisplay").innerText = (startingBet);
         document.getElementById("rolls").innerText = totalRolls;
-        document.getElementById("highestRollCountDisplay").innerText = rollCountAtHighestWon;
-        document.getElementById("highestWonDisplay").innerText = highestWon;
-        document.getElementById("resultsDisplay").innerText = (sum);
+        document.getElementById("highestRollCountDisplay").innerText = rollCountFinal;
+        document.getElementById("highestWonDisplay").innerText = highWonFinal;
         document.getElementById("submitButton").innerText = "Play Again?";
         return false;
     }
