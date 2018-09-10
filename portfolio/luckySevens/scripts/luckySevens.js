0ef51577a30125dbc2b0a056ca0df60e47206ff8
iamlejon <niquelejon@gmail.com>
2018-09-10 14:38:25 -0500

    Updated highest Amount won to include starting bet. Also added edge case logic for highest amount won, changed variable playerAmount to accountBalance for better clarity, and added comments throughout code for understanding.

--- portfolio/luckySevens/scripts/luckySevens.js
+++ portfolio/luckySevens/scripts/luckySevens.js
@@ -1,32 +1,38 @@
-var playerAmount;
-var startingBet;
+var accountBalance; // The Amount player has at all times
+var startingBet; // The amount player is starting with
+
+/*This function asks user to enter bet in dollars, fixes the starting bet to 2 decimal places and converts it back to a number since .toFixed converts to a string.
+  The parseFloat is used to prevent bugs and ensure value is always a number not a string for math operation purposes
+  If user enters a non numeric number, it will display error and user will try again*/
 function placeBetGreeting() {
-    startingBet = parseFloat(prompt("Enter your bet")).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
-    playerAmount = parseFloat(startingBet);
-    if(playerAmount <= 0 || playerAmount=="" || isNaN(playerAmount)){
+    startingBet = parseFloat(Math.round(prompt("Enter your bet in $ Dollars"))).toFixed(2);
+    accountBalance = parseFloat(startingBet);
+    if(accountBalance <= 0 || accountBalance=="" || isNaN(accountBalance)){
         alert("You must enter a numeric number greater than 0")
         placeBetGreeting();
         return false;
     } else {
-        document.getElementById("startingBet").innerText = startingBet;
+        document.getElementById("startingBet").innerText = "$"+startingBet;
         return false;
     }
     
 }
 
-var diceA = 0;
-var diceB = 0;
-var sum;
-var totalRolls = 0;
-var winnings = 0;
-var pairs = {};
+var diceA = 0; // Dice number 1
+var diceB = 0; // Dice number 2
+var sum; // the sum of two dices added together
+var currentTotalRolls = 0; // To keep track of roll count per round
+var pairs = {}; // This will house they key value relationship between the current roll count and current amount held
+var sumEachRound = []; //This is to self QA check in console that sum is accurate in relation to player's current amount
 
+
+//The clear all function will clear all totals, objects, arrays, and displays so that everything is reset for a new round, then will call the placeBetGreeting.
 function clearAll(){
-    totalRolls=0;
+    currentTotalRolls=0;
     startingBet=0;
     sum=0;
-    winnings=0;
     pairs = {};
+    sumEachRound = [];
     document.getElementById("results").style.display = "none";
     document.getElementById("submitButton").innerText = "Play";
     placeBetGreeting();
@@ -35,41 +41,44 @@ function clearAll(){
 
 
 function rollDice() {
-    if(playerAmount==0) {
-        clearAll();
+    if(accountBalance==0) { //When user clicks play again button, if player amount is 0 it resets all data for new round.
+        clearAll(); // After data is reset user will be prompted for new bet and function will continue with calculations below.
     }
-        while(Number(playerAmount)>Number(0)){
+        while(Number(accountBalance) > Number(0)){
             diceA = Math.floor(Math.random() * 6) + 1;
             diceB = Math.floor(Math.random() * 6) + 1;
-            sum = diceA+diceB;
+            sum = diceA+diceB; // Takes the sum of two dice rolled.
 
             if(sum==7) {
-                playerAmount = parseFloat(playerAmount) + parseFloat(4.00);
-                winnings += 4;
-                
+                accountBalance = parseFloat(accountBalance) + parseFloat(4.00);
             }
             else {
-                playerAmount = parseFloat(playerAmount) - parseFloat(1.00);
-                winnings -= 1;
+                accountBalance = parseFloat(accountBalance) - parseFloat(1.00);
             }
         
-            totalRolls++;
-            pairs[totalRolls] = winnings;
+            currentTotalRolls++;
+            pairs[currentTotalRolls] = accountBalance; //Key Value relationship between currentTotalRolls and accountBalance
+            sumEachRound.push(sum); //this pushes dice sum to an array for QA Self checking purposes to ensure correct data/flow
         }
 
     
-       
+       //Code to sort the pairs in ascending order and capture the highest Amount won along with it's roll Count at that position
         let keys = Object.keys(pairs);
         keys.sort(function(a,b){return pairs[b]-pairs[a];});
         let rollCountAtHighestWon = keys[0];
         let highestWon = pairs[rollCountAtHighestWon];
 
-        var rollCountFinal = Object.is(rollCountAtHighestWon, undefined) ? 0 : rollCountAtHighestWon;
-        var highWonFinal = Object.is(highestWon, undefined) ? 0 : highestWon;
+
+        //These final highest won and roll counts cover edge cases.  For instance if the highest amount won is less than the starting bet, then the starting bet is the high amount held
+        //Example: If I Bet $3.00 but never rolled a 7, $3.00 at roll 0 should be highest won and highest roll count, not $2.00 at roll 1.
+
+        var highWonFinal = highestWon < startingBet ? startingBet : highestWon;
+        var rollCountFinal = highestWon < startingBet ? 0 : rollCountAtHighestWon;
+        
        
         document.getElementById("results").style.display = "block";
         document.getElementById("startingBetDisplay").innerText = (startingBet);
-        document.getElementById("rolls").innerText = totalRolls;
+        document.getElementById("rolls").innerText = currentTotalRolls;
         document.getElementById("highestRollCountDisplay").innerText = rollCountFinal;
         document.getElementById("highestWonDisplay").innerText = highWonFinal;
         document.getElementById("submitButton").innerText = "Play Again?";
