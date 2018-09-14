a86de61c4b9892b68048ade8852907a5c9e1af92
iamlejon <niquelejon@gmail.com>
2018-09-13 21:34:51 -0500

    Added a validate Form function using regex tests to make sure name, email, and phone numbers are valid

---
+++ portfolio/restaurant-site3/scripts/contactform.js
@@ -0,0 +1,24 @@
+function validateForm() {
+    var customerName = document.forms["contactForm"]["name"].value;
+    var customerEmail = document.forms["contactForm"]["email"].value;
+    var customerPhone = document.forms["contactForm"]["phone"].value;
+    var phoneNumberPattern = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;
+    var emailPattern = /\S+@\S+\.\S+/;
+
+
+    if (customerName == "") {
+        alert("Name must be filled out");
+        return false;
+    }
+    // This checks to see if email is in a valid format
+    else if (emailPattern.test(customerEmail) == false) {
+            alert("Email is invalid. Please a valid email. Ex. someone@something.domain");
+            return false;
+    }
+    // This checks to see if phone number is 10 digits, ignores formatting
+    else if ((customerPhone.match(/\d/g).length!==10)) {
+        alert("Phone number invalid.  Please enter a valid 10 digit phone number");
+        return false;
+    }
+    
+}
